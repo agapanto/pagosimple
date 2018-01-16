041f5a7bb54f1f6711bc92507981f35bc63ce437
mcueto <cueto@live.cl>
2018-01-15 22:27:21 -0300

    Add .env.dist file

--- pagosimple/settings.py
+++ pagosimple/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'pagosimple.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRESQL_NAME'),
+        'USER': os.environ.get('POSTGRESQL_USER'),
+        'PASSWORD': os.environ.get('POSTGRESQL_PASSWORD'),
+        'HOST': os.environ.get('POSTGRESQL_HOST'),
+        'PORT': os.environ.get('POSTGRESQL_PORT'),
     }
 }
 
