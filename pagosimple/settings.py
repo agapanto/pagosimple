2e334e262b2efb149339bdb9bdaee0a24b36297f
mcueto <cueto@live.cl>
2018-03-31 03:23:11 -0300

    Create Product & ProductVariant models

--- pagosimple/settings.py
+++ pagosimple/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework_apicontrol',
     'accounts',
     'plans',
+    'products',
 ]
 
 MIDDLEWARE = [
--- products/models.py
+++ products/models.py
@@ -1,3 +1,97 @@
+"""plans app models."""
+import logging
+# from dateutil.relativedelta import relativedelta
 from django.db import models
+# from django.contrib.postgres.fields import (
+#     JSONField,
+# )
+from rest_framework_apicontrol.mixins import (
+    # ActiveModelMixin,
+    # EnabledModelMixin,
+    PerAppModelMixin,
+    TrackableModelMixin,
+    UniqueIDModelMixin,
+)
+from rest_framework_apicontrol.models import (
+    CODE_FIELD_MAX_LENGTH,
+    NAME_FIELD_MAX_LENGTH,
+)
 
-# Create your models here.
+logger = logging.getLogger(__name__)
+
+
+class BaseProduct(PerAppModelMixin,
+                  TrackableModelMixin,
+                  UniqueIDModelMixin):
+    """
+    BaseProduct model.
+
+    Contains the common fields between Product and ProductVariant models.
+    """
+
+    code = models.CharField(
+        max_length=CODE_FIELD_MAX_LENGTH
+    )
+    name = models.CharField(
+        max_length=NAME_FIELD_MAX_LENGTH
+    )
+    description = models.TextField(
+        blank=True
+    )
+    slug = models.SlugField()
+
+    class Meta:
+        """Make the base model abstract."""
+
+        abstract = True
+
+    def __str__(self):
+        """Return the class instance item name in django admin."""
+        return str(self.name)+" - "+str(self.created_at)
+
+
+class Product(BaseProduct):
+    """
+    Product model.
+
+    Represents a Plan in an specific app.
+    """
+
+    class Meta:
+        """
+        Set contraints to only allow the following.
+
+        - One account Product code per App
+        """
+
+        unique_together = (("code", "app"),)
+
+    def __str__(self):
+        """Return the class instance item name in django admin."""
+        return str(self.name)+" - "+str(self.created_at)
+
+
+class ProductVariant(BaseProduct):
+    """
+    Product model.
+
+    Represents a Plan in an specific app.
+    """
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        """
+        Set contraints to only allow the following.
+
+        - One account Product code per App
+        """
+
+        unique_together = (("code", "app"),)
+
+    def __str__(self):
+        """Return the class instance item name in django admin."""
+        return str(self.name)+" - "+str(self.created_at)
