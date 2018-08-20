e9e6954580f40ed804bb57cb427f2bca7e72e231
mcueto <cueto@live.cl>
2018-08-19 22:43:36 -0300

    Bugfix: loging & logout urls in main app

--- pagosimple_frontend/urls.py
+++ pagosimple_frontend/urls.py
@@ -30,12 +30,12 @@ urlpatterns = [
     ),
     path(
         'login',
-        auth_views.login,
+        auth_views.LoginView.as_view(),
         name='login'
     ),
     path(
         'logout',
-        auth_views.logout,
+        auth_views.LogoutView.as_view(),
         {'next_page': '/web'},
         name='logout'
     ),
