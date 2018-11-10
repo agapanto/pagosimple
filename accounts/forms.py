b2e8aa18891d2f3bd2b2e4121bcfec2f95f66718
mcueto <cueto@live.cl>
2018-11-10 05:36:41 -0300

    Add clean_customer_id & clean_email functions to AccountForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,8 @@
 """plans app forms."""
+from django import forms
+from django.core.exceptions import (
+    ValidationError,
+)
 from django.forms import (
     # Form,
     ModelForm,
@@ -31,3 +35,49 @@ class AccountForm(ModelForm):
         widgets = {
             'app': forms.HiddenInput()
         }
+
+    def clean_customer_id(self):
+        """Validate & clean the customer_id form field."""
+        # Get data to look at
+        customer_id = self.cleaned_data["customer_id"]
+        app = self.cleaned_data["app"]
+
+        query_args = {
+            'customer_id': customer_id,
+            'app': app
+        }
+
+        if Account.objects.filter(**query_args).exists():
+            # Get the existant model instance
+            existant_account = Account.objects.get(**query_args)
+
+            # Compare if the current instance model is the one is saved in DB
+            if self.instance.pk != existant_account.pk:
+                raise ValidationError(
+                    'An Account with this customer id already exists in current App.'
+                )
+
+        return customer_id
+
+    def clean_email(self):
+        """Validate & clean the email form field."""
+        # Get data to look at
+        email = self.cleaned_data["email"]
+        app = self.cleaned_data["app"]
+
+        query_args = {
+            'email': email,
+            'app': app
+        }
+
+        if Account.objects.filter(**query_args).exists():
+            # Get the existant model instance
+            existant_account = Account.objects.get(**query_args)
+
+            # Compare if the current instance model is the one is saved in DB
+            if self.instance.pk != existant_account.pk:
+                raise ValidationError(
+                    'An Account with this email already exists in current App.'
+                )
+
+        return email
